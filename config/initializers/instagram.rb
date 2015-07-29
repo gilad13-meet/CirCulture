fa769370838c572b27dfa5554cae2c1bc5b54eb3
Gilad Kleinman <dkgs1998@gmail.com>
2015-07-29 18:04:35 +0300

    added instagram

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require "instagram"
+
+    Instagram.configure do |config|
+
+    config.client_id = "242aee9a0d2c4954a72d3f12b58a7716"
+
+    config.access_token = "299639420.242aee9.eb99b2dc05bd4c5b989ae77a39ef2c63"
+    end
