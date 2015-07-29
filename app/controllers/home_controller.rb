ab39e8b2d0d9127af45001bb8279958820b61f0f
Gilad Kleinman <dkgs1998@gmail.com>
2015-07-29 18:31:46 +0300

    added boostrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
-  	ramallah = Instagram.location_search(31.898043, 35.204271).first(3)
-  	jerusalem = Instagram.location_search(31.768319, 35.21371).first(3)
+  	# ramallah = Instagram.location_search(31.898043, 35.204271).first(3)
+  	# tel_aviv = Instagram.location_search(31.768319, 35.21371).first(3)
 
-  	@ramallah_photos = []
-  	remmallah.each do |location|
-  	end
+  	# @ramallah_photos = []
+  	# remmallah.each do |location|
+  	# 	locaiton
+  	# end
 
   end
 end
