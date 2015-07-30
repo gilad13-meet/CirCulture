f753c2d4d9ce974dea179d2182ab6657218c3ebc
Gilad Kleinman <dkgs1998@gmail.com>
2015-07-30 15:24:33 +0300

    added better files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,44 +1,27 @@
 require 'json'
+include ActionView::Helpers::OutputSafetyHelper
 class HomeController < ApplicationController
   def index
-  	# remmallah = Instagram.location_search(31.898043, 35.204271)
+   # pale = Instagram.tag_recent_media("palestinianfood")
+   # isra = Instagram.tag_recent_media("israelifood")
 
-  	# @ramallah_photos = []
-   #  count_photos = 0
-   #  i = 0
-   #  while count_photos < 5
-   #  	location = remmallah[i]
-  	# 	Instagram.location_recent_media(location.id).each do |media|
-  	# 		if media["type"] == "image" and count_photos < 5
-  	# 			@ramallah_photos << media["images"]["standard_resolution"]["url"]
-   #        count_photos+=1
-  	# 		end
-  	# 	end
-   #    i+=1
-   #  end
+   # i_photos = []
+   # isra.each do |media|
+   #   i_photos << media["images"]["standard_resolution"]["url"]
+   # end
 
-  	# tel_aviv = Instagram.location_search(32.085300, 34.781768)
-  	# @tel_aviv_photos = []
-   #  count_photos = 0
-   #  i = 0
-   #  while count_photos < 5
-   #    location = tel_aviv[i]
-  	# 	Instagram.location_recent_media(location.id).each do |media|
-  	# 		if media["type"] == "image" and count_photos < 5
-  	# 			@tel_aviv_photos << media["images"]["standard_resolution"]["url"]
-   #        count_photos += 1
-  	# 		end
-  	# 	end
-   #    i+=1
-  	# end
+   # p_photos = []
+   # pale.each do |media|
+   # 	 p_photos << media["images"]["standard_resolution"]["url"]
+   # end
 
-   #  puts @photos = {"Tel Aviv" => @tel_aviv_photos, "Remmallah" => @ramallah_photos}.to_json
+   # hash = {"Israel" => i_photos, "Palestine" => p_photos}
 
+   # @stuff = hash.to_json
 
    file = File.read(Rails.root + 'public/locations_images.JSON')
    data_hash = JSON.parse(file)
-   puts data_hash.keys
-   @tel_aviv_photos = data_hash["Tel Aviv"]
-   @ramallah_photos = data_hash["Remmallah"]
+   @israel_photos = data_hash["Israel"]
+   @palestine_photos = data_hash["Palestine"]
   end
 end
