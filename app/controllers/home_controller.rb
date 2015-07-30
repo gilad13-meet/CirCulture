b95ee43ca83f85d4d58edb71954f0446706150ab
Gilad Kleinman <dkgs1998@gmail.com>
2015-07-30 12:38:26 +0300

    added instagram

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,34 @@
 class HomeController < ApplicationController
   def index
-  	# ramallah = Instagram.location_search(31.898043, 35.204271).first(3)
-  	# tel_aviv = Instagram.location_search(31.768319, 35.21371).first(3)
+  	remmallah = Instagram.location_search(31.898043, 35.204271)
 
-  	# @ramallah_photos = []
-  	# remmallah.each do |location|
-  	# 	locaiton
-  	# end
+  	@ramallah_photos = []
+    count_photos = 0
+    i = 0
+    while count_photos < 5
+    	location = remmallah[i]
+  		Instagram.location_recent_media(location.id).each do |media|
+  			if media["type"] == "image" and count_photos < 5
+  				@ramallah_photos << media["images"]["standard_resolution"]["url"]
+          count_photos+=1
+  			end
+  		end
+      i+=1
+    end
 
+  	tel_aviv = Instagram.location_search(32.085300, 34.781768)
+  	@tel_aviv_photos = []
+    count_photos = 0
+    i = 0
+    while count_photos < 5
+      location = tel_aviv[i]
+  		Instagram.location_recent_media(location.id).each do |media|
+  			if media["type"] == "image" and count_photos < 5
+  				@tel_aviv_photos << media["images"]["standard_resolution"]["url"]
+          count_photos += 1
+  			end
+  		end
+      i+=1
+  	end
   end
 end
