22c75ee3aae445589e8d166b2e42cbe89ad25516
Rami Manna <rami15manna@gmail.com>
2015-08-02 11:17:47 +0300

    adding folder for html page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get ':controller(/:action(/:id(.:format)))' => 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
